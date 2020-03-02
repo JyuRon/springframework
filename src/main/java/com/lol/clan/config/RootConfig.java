8e754c288d5c4e2e344690ee89d69fecd8e7d9f7
user <bebuldugi@icloud.com>
2020-03-02 20:48:05 +0900

    DB, Lombok, log4j 레벨 설정, XML Mapper설정, log4jdbc, hikaricp, mybatis

--- src/main/java/com/lol/clan/config/RootConfig.java
+++ src/main/java/com/lol/clan/config/RootConfig.java
@@ -1,10 +1,58 @@
 package com.lol.clan.config;
 
+
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 @Configuration
 @ComponentScan(basePackages= {"com.lol.clan.sample"})
+@MapperScan(basePackages= {"com.lol.clan.mapper"})
 public class RootConfig {
+	
+	
+	//커넥션 풀 설정 : 동시접속
+	@Bean
+	public DataSource dataSource() {
+		
+		HikariConfig hikariConfig = new HikariConfig();
+		//hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+		//hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:orcl");
+		
+		
+		//log4j 설정
+		hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+		hikariConfig.setJdbcUrl("jdbc:log4jdbc:oracle:thin:@localhost:1521:orcl");
+		
+		
+		hikariConfig.setUsername("dodam");
+		hikariConfig.setPassword("1234");
+		
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+		
+		return dataSource;
+	}
+	
+	
+	
+	//Mybatis
+	@Bean
+	public SqlSessionFactory sqlSessionFactory() throws Exception{
+		
+		SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+		sqlSessionFactory.setDataSource(dataSource());
+		
+		return (SqlSessionFactory) sqlSessionFactory.getObject();
+	}
+	
 
 }
--- src/test/java/com/lol/clan/persistence/TimeMapperTest.java
+++ src/test/java/com/lol/clan/persistence/TimeMapperTest.java
@@ -1,33 +1,37 @@
-package com.lol.clan.sample;
+package com.lol.clan.persistence;
 
-import static org.junit.Assert.assertNotNull;
+import javax.sql.DataSource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.lol.clan.config.RootConfig;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.lol.clan.mapper.TimeMapper;
+
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes= {RootConfig.class})
 @Log4j
-public class SampleTests {
+public class TimeMapperTest {
+
 	
 	@Setter(onMethod_ = {@Autowired})
-	private Restaurant restaurant;
+	private TimeMapper timeMapper;
 	
 	@Test
-	public void testExist() {
-		assertNotNull(restaurant);
-		
-		log.info(restaurant);
-		log.info("-----------------");
-		log.info(restaurant.getChef());
-		
+	public void testGetTime() {
+		log.info(timeMapper.getClass().getName());
+		log.info(timeMapper.getTime());
 	}
-
+	
+	@Test
+	public void testGetTime2() {
+		log.info("getTime2");
+		log.info(timeMapper.getTime2());
+	}
 }
